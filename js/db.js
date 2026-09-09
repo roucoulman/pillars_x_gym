ce08154489d8821a3c26a0f8c51cca899d49dad5
roucoulman <jonathan.morin1210@gmail.com>
2026-09-09 23:29:38 +0200

    push prout

--- js/db.js
+++ js/db.js
@@ -43,7 +43,6 @@ export async function initialiserSQLite() {
 
   // Création des tables (sans DROP TABLE)
   db.run(`
-    DROP TABLE muscles ;
     CREATE TABLE IF NOT EXISTS muscles (
       id TEXT PRIMARY KEY,
       name TEXT NOT NULL,
